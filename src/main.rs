f055c13492f0233e2c8ecbf64bb8ea9773fa8ab8
Folafunmi <folafunmimustapha@gmail.com>
2022-11-15 15:03:56 +0100

    bind a port

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,8 +11,9 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                stream.write("PING\r\n".as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
